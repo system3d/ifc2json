e440d659252e8de71dd6b654e3f8bdcfb1a814e6
Luciano <tonetlds@gmail.com>
2016-06-01 00:11:58 -0300

    Working beta \o/

--- example/example4.php
+++ example/example4.php
@@ -5,4 +5,4 @@ use System3D\IFC2JSON\IFC2JSON;
 
 $IFC2JSON = new IFC2JSON( "model.ifc", false );
 
-echo $IFC2JSON->getJson();
+print_r( $IFC2JSON->getJson() );
--- example/example7.php
+++ example/example7.php
@@ -6,4 +6,4 @@ use System3D\IFC2JSON\IFC2JSON;
 
 $IFC2JSON = new IFC2JSON( "model.ifc", true );
 
-print_r( $IFC2JSON->getJson() );
+dump( $IFC2JSON->getJson() );
